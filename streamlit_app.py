686cc76f95f441e830ec25e163104f3f13337174
Richard Croker <ra.croker@gmail.com>
2026-03-05 00:09:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,7 +146,7 @@ st.dataframe(top_cost_data)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.write(df1)
+    st.dataframe(top_items_data)
 
 with col2:
-    st.write(df2)
+    st.dataframe(top_cost_data)
